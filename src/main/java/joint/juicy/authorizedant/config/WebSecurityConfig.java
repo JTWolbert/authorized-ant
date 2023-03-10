dc75d3da00eb03998783537ecc37b14e29def911
JTWolbert <jtwolbertee@gmail.com>
2023-03-09 20:41:14 -0500

    websocket service

--- src/main/java/joint/juicy/authorizedant/config/WebSecurityConfig.java
+++ src/main/java/joint/juicy/authorizedant/config/WebSecurityConfig.java
@@ -17,8 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
@@ -56,6 +54,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests().requestMatchers("/user/login", "/user/create").permitAll()
